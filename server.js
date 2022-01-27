8af96d917a6cf0a6ffb407e27556d525099c5c4d
Sean <seanminches@gmail.com>
2022-01-26 21:47:36 -0500

    its WORKING

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const noteData = require("./db/db.json"); // Helper method for generating unique ids
-//const uuid = require("./helpers/uuid");
+const uuid = require("./helpers/uuid");
+const {readAndAppend} = require('./helpers/fsUtils');
 
 const PORT = process.env.PORT || 3001;
 
@@ -23,10 +24,29 @@ app.get("/notes", (req, res) =>
 app.get("/api/notes", (req, res) => res.json(noteData));
 app.get("/api/notes/:note_id", (req, res) => res.json(noteData));
 
-// GET Route for wildcard page
+
 app.get("*", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
+
+app.post('/api/notes', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const note = {
+      title,
+      text,
+      id: uuid(),
+    };
+    noteData.push(note)
+    readAndAppend(note, './db/db.json');
+    res.json(`Tip added successfully 🚀`);
+  } else {
+    res.error('Error in adding tip');
+  }
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
